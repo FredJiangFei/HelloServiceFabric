e1bd40421b98f1f364c84506c99f6fca607e2bbf
Fred <jiangf@shinetechchina.com>
2017-10-05 12:41:35 +0800

    Employee activity present

---
+++ Backend/StatefulBackendService/Controllers/EmployeeController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
+
+namespace StatefulBackendService.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        private readonly IReliableStateManager _stateManager;
+
+        public EmployeeController(IReliableStateManager stateManager)
+        {
+            this._stateManager = stateManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var ct = new CancellationToken();
+
+            var votesDictionary = await _stateManager.GetOrAddAsync<IReliableDictionary<string, int>>("employees");
+
+            using (var tx = _stateManager.CreateTransaction())
+            {
+                var list = await votesDictionary.CreateEnumerableAsync(tx);
+
+                var enumerator = list.GetAsyncEnumerator();
+
+                var result = new List<KeyValuePair<string, int>>();
+                while (await enumerator.MoveNextAsync(ct))
+                {
+                    result.Add(enumerator.Current);
+                }
+
+                return Json(result);
+            }
+        }
+
+        // PUT api/VoteData/name
+        [HttpPut("{name}")]
+        public async Task<IActionResult> Put(string name)
+        {
+            var votesDictionary = await _stateManager.GetOrAddAsync<IReliableDictionary<string, int>>("employees");
+
+            using (var tx = _stateManager.CreateTransaction())
+            {
+                await votesDictionary.AddOrUpdateAsync(tx, name, 1, (key, oldvalue) => oldvalue + 1);
+                await tx.CommitAsync();
+            }
+
+            return new OkResult();
+        }
+
+        // DELETE api/VoteData/name
+        [HttpDelete("{name}")]
+        public async Task<IActionResult> Delete(string name)
+        {
+            var votesDictionary = await _stateManager.GetOrAddAsync<IReliableDictionary<string, int>>("counts");
+
+            using (var tx = _stateManager.CreateTransaction())
+            {
+                if (!await votesDictionary.ContainsKeyAsync(tx, name))
+                    return new NotFoundResult();
+
+                await votesDictionary.TryRemoveAsync(tx, name);
+                await tx.CommitAsync();
+                return new OkResult();
+            }
+        }
+    }
+}
--- Backend/WebService/Controllers/EmployeeController.cs
+++ Backend/WebService/Controllers/EmployeeController.cs
@@ -1,27 +1,88 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Fabric;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace WebService.Controllers
 {
     [Route("api/[controller]")]
-    public class EmployeeController
+    public class EmployeeController : Controller
     {
-        private readonly ConfigSettings _configSettings;
         private readonly HttpClient _httpClient;
+        private readonly StatelessServiceContext _serviceContext;
+        private readonly ConfigSettings _configSettings;
+        string partitionKind = "Int64Range";
+        string partitionKey = "0";
 
-        public EmployeeController(StatelessServiceContext serviceContext, 
-            FabricClient fabricClient, 
-            ConfigSettings configSettings, 
-            HttpClient httpClient)
+        public EmployeeController(HttpClient httpClient,
+            StatelessServiceContext serviceContext,
+            ConfigSettings configSettings)
         {
-            _configSettings = configSettings;
             _httpClient = httpClient;
+            _serviceContext = serviceContext;
+            _configSettings = configSettings;
+        }
+        private string GetServiceUri()
+        {
+            return _serviceContext.CodePackageActivationContext.ApplicationName +
+                "/" + this._configSettings.StatefulBackendServiceName +
+                "/api/Employee";
+        }
+
+        // GET: api/Employee
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var url = GetServiceUri();
+            var response = await _httpClient.GetAsync($"{url}?PartitionKind={partitionKind}&PartitionKey={partitionKey}");
+
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                return this.StatusCode((int)response.StatusCode);
+            }
+
+            var votes = JsonConvert.DeserializeObject<List<KeyValuePair<string, int>>>(await response.Content.ReadAsStringAsync());
+            return Json(votes);
+        }
+
+        // PUT: api/Employee/name
+        [HttpPut("{name}")]
+        public async Task<IActionResult> Put(string name)
+        {
+            var putContent = StringContent(name);
+            var url = GetServiceUri();
+            var proxyUrl = $"{url}/{name}?PartitionKind={partitionKind}&PartitionKey={partitionKey}";
+
+            var response = await _httpClient.PutAsync(proxyUrl, putContent);
+            return new ContentResult
+            {
+                StatusCode = (int)response.StatusCode,
+                Content = await response.Content.ReadAsStringAsync()
+            };
+        }
+
+        private static StringContent StringContent(string name)
+        {
+            string payload = $"{{ 'name' : '{name}' }}";
+            StringContent putContent = new StringContent(payload, Encoding.UTF8, "application/json");
+            putContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            return putContent;
+        }
+
+        // DELETE: api/Employee/name
+        [HttpDelete("{name}")]
+        public async Task<IActionResult> Delete(string name)
+        {
+            var url = GetServiceUri();
+            var response = await _httpClient.DeleteAsync($"{url}/{name}?PartitionKind={partitionKind}&PartitionKey={partitionKey}");
+
+            return response.StatusCode != System.Net.HttpStatusCode.OK
+                ? StatusCode((int)response.StatusCode)
+                : new OkResult();
         }
     }
 }
